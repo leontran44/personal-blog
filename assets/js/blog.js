f20264298bbd4bde6874a5c09a049a8238beb863
Leon <tranhoangquan44@gmail.com>
2024-06-29 16:16:33 +1000

    added createElementAndAppend() function to blog.js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,7 +1,12 @@
 // TODO: Create a variable that selects the main element
+const main = document.querySelector("main");
 
 // TODO: Create a function that builds an element and appends it to the DOM
-
+function createElementAndAppend(tag, text, parent) {
+    const element = document.createElement(tag);
+    element.textContent = text;
+    parent.appendChild(element);
+}
 // TODO: Create a function that handles the case where there are no blog posts to display
 
 // TODO: Create a function that reads from local storage and returns the data
